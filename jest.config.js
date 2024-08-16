ef7127c34f211f0a1d407a67dd21a44c3b3403b4
Patrick Badiang <patrickvyn@gmail.com>
2024-08-16 14:17:54 -0400

    Added jest file

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    moduleNameMapper: {
+      // Other mappers if needed
+    },
+    preset: 'esdom', // Adjust this preset based on your testing needs
+    testEnvironment: 'jsdom', // Consider using 'jest-environment-jsdom' for more control
+  };
