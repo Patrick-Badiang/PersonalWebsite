442ec6ee8ffac294e59e9f8f469776680bae9b53
Patrick Badiang <patrickvyn@gmail.com>
2024-08-16 15:15:37 -0400

    Changed

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -19,7 +19,7 @@ import {
   Typography,
   useMediaQuery,
 } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2";
+import Grid from "@mui/material/Unstable_Grid2/Grid2.js";
 // import Buttons from './Buttons';
 import React from "react";
 
