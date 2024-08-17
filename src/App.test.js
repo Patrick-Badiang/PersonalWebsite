1e7db6c242cb02b1aca99c5de48c4eaabae684f3
Patrick Badiang <patrickvyn@gmail.com>
2024-08-16 22:19:57 -0400

    Resolving warnings

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App.js';
-
+import { render } from '@testing-library/react';
 import { act } from 'react';
+import App from './App.js';
 
 test('renders learn react link', () => {
   act(() => {
     render(<App />);
   });
-  
-
 });
