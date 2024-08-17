be1ab60ede6e01111bbc004392c17e896a95a69c
Patrick Badiang <patrickvyn@gmail.com>
2024-08-16 23:00:46 -0400

    Fixed unused code

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -10,32 +10,32 @@
  *      A rectangle
  */
 
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  IconButton,
-  Typography,
-  useMediaQuery,
-} from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2/Grid2.js";
-// import Buttons from './Buttons';
-import React from "react";
+// import {
+//   Box,
+//   Button,
+//   Card,
+//   CardContent,
+//   IconButton,
+//   Typography,
+//   useMediaQuery,
+// } from "@mui/material";
+// import Grid from "@mui/material/Unstable_Grid2/Grid2.js";
+// // import Buttons from './Buttons';
+// import React from "react";
 
-import EmailIcon from "@mui/icons-material/Email.js";
-import LocationOnIcon from "@mui/icons-material/LocationOn.js";
-import FileDownloadIcon from "@mui/icons-material/FileDownload.js";
+// import EmailIcon from "@mui/icons-material/Email.js";
+// import LocationOnIcon from "@mui/icons-material/LocationOn.js";
+// import FileDownloadIcon from "@mui/icons-material/FileDownload.js";
 
-import LinkedInIcon from "@mui/icons-material/LinkedIn.js";
-import GitHubIcon from "@mui/icons-material/GitHub.js";
-import InstagramIcon from "@mui/icons-material/Instagram.js";
+// import LinkedInIcon from "@mui/icons-material/LinkedIn.js";
+// import GitHubIcon from "@mui/icons-material/GitHub.js";
+// import InstagramIcon from "@mui/icons-material/Instagram.js";
 
-import Pdf from "../Documents/Resume.pdf";
+// import Pdf from "../Documents/Resume.pdf";
 
 function Intro() {
   //Checking if the  screen is small
-  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("md"));
+  // const isSmall = useMediaQuery((theme) => theme.breakpoints.down("md"));
 
   //Gif
 
