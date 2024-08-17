48925b956bc3c004f56040a206590fd7a33af5bd
Patrick Badiang <patrickvyn@gmail.com>
2024-08-16 23:21:26 -0400

    Trying Name Email and icons

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -15,21 +15,21 @@ import {
   // Button,
   Card,
   CardContent,
-  // IconButton,
-  // Typography,
+  IconButton,
+  Typography,
   useMediaQuery,
 } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2.js";
 // import Buttons from './Buttons';
 import React from "react";
 
-// import EmailIcon from "@mui/icons-material/Email.js";
+import EmailIcon from "@mui/icons-material/Email.js";
 // import LocationOnIcon from "@mui/icons-material/LocationOn.js";
 // import FileDownloadIcon from "@mui/icons-material/FileDownload.js";
 
-// import LinkedInIcon from "@mui/icons-material/LinkedIn.js";
-// import GitHubIcon from "@mui/icons-material/GitHub.js";
-// import InstagramIcon from "@mui/icons-material/Instagram.js";
+import LinkedInIcon from "@mui/icons-material/LinkedIn.js";
+import GitHubIcon from "@mui/icons-material/GitHub.js";
+import InstagramIcon from "@mui/icons-material/Instagram.js";
 
 // import Pdf from "../Documents/Resume.pdf";
 
@@ -73,6 +73,93 @@ function Intro() {
                 />
               </Box>
             </Grid>
+            {/* //Name, Email, Location */}
+            <Grid container direction={"column"} xs={7} spacing={0}>
+              {/* //Name */}
+              <Grid item xs={12}>
+                <Typography variant="h5" textAlign={"start"}>
+                  Patrick Vyn Badiang
+                </Typography>
+              </Grid>
+              {/* //Email with Email Icon*/}
+
+              <Grid container direction={"row"} xs={12} spacing = {0}>
+                  <EmailIcon
+                    sx={{
+                      color: "white",
+                      height: isSmall ? "8px" : "16px",
+                    }}
+                  />
+                  <Typography textAlign={"start"} variant="body1">
+                    patrick.vyn.llanto@gmail.com
+                  </Typography>
+                
+              </Grid>
+              {/* //Description */}
+              <Typography variant="body2" textAlign={"start"} color="#FFD3AB">
+                Full Stack Engineer | Undergraduate Student
+              </Typography>
+              {/* //Icon Buttons */}
+              <div style={{ marginTop: "15px" }}>
+                <Grid container direction={"row"} justifyContent={"start"}>
+                  <IconButton
+                    href="https://www.linkedin.com/in/patrick-vyn-badiang/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Example"
+                    sx={{
+                      height: isSmall ? "8px" : "32px",
+                      aspectRatio: "1/1",
+                    }}
+                  >
+                    <LinkedInIcon
+                      sx={{
+                        color: "#000000",
+                        height: isSmall ? "8px" : "32px",
+                        aspectRatio: "1/1",
+                      }}
+                    />
+                  </IconButton>
+                  <IconButton
+                    href="https://www.instagram.com/patrick.vyn/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Example"
+                    sx={{
+                      height: isSmall ? "8px" : "32px",
+                      aspectRatio: "1/1",
+                    }}
+                  >
+                    <InstagramIcon
+                      sx={{
+                        color: "#000000",
+                        height: isSmall ? "8px" : "32px",
+                        aspectRatio: "1/1",
+                      }}
+                    />
+                  </IconButton>
+                  <IconButton
+                    href="https://github.com/Patrick-Badiang"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Example"
+                    sx={{
+                      height: isSmall ? "8px" : "32px",
+                      aspectRatio: "1/1",
+                    }}
+                  >
+                    <GitHubIcon
+                      sx={{
+                        color: "#000000",
+                        height: isSmall ? "8px" : "32px",
+                        aspectRatio: "1/1",
+                      }}
+                    />
+                  </IconButton>
+                </Grid>
+              </div>
+            </Grid>
+
             
           </Grid> 
         </CardContent>
