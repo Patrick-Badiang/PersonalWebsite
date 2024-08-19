a0e6a575cf557a82c6e9d33a9c268ed2924a76e6
Patrick Badiang <patrickvyn@gmail.com>
2024-08-19 14:04:58 -0400

    Removed type module in  package and changes email.js to email

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -24,7 +24,7 @@ import Grid from "@mui/material/Unstable_Grid2/Grid2.js";
 import React from "react";
 
 //The ".js" is  messing up the import
-import EmailIcon from "@mui/icons-material/Email.js";
+import EmailIcon from "@mui/icons-material/Email";
 import LocationOnIcon from "@mui/icons-material/LocationOn.js";
 import FileDownloadIcon from "@mui/icons-material/FileDownload.js";
 
@@ -97,129 +97,8 @@ function Intro() {
                   </Typography>
                 
               </Grid>
-              {/* //Description */}
-              <Typography variant="body2" textAlign={"start"} color="#FFD3AB">
-                Full Stack Engineer | Undergraduate Student
-              </Typography>
-              {/* //Icon Buttons */}
-              <div style={{ marginTop: "15px" }}>
-                <Grid container direction={"row"} justifyContent={"start"}>
-                  <IconButton
-                    href="https://www.linkedin.com/in/patrick-vyn-badiang/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Example"
-                    sx={{
-                      height: isSmall ? "8px" : "32px",
-                      aspectRatio: "1/1",
-                    }}
-                  >
-                    <LinkedInIcon
-                      sx={{
-                        color: "#000000",
-                        height: isSmall ? "8px" : "32px",
-                        aspectRatio: "1/1",
-                      }}
-                    />
-                  </IconButton>
-                  <IconButton
-                    href="https://www.instagram.com/patrick.vyn/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Example"
-                    sx={{
-                      height: isSmall ? "8px" : "32px",
-                      aspectRatio: "1/1",
-                    }}
-                  >
-                    <InstagramIcon
-                      sx={{
-                        color: "#000000",
-                        height: isSmall ? "8px" : "32px",
-                        aspectRatio: "1/1",
-                      }}
-                    />
-                  </IconButton>
-                  <IconButton
-                    href="https://github.com/Patrick-Badiang"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Example"
-                    sx={{
-                      height: isSmall ? "8px" : "32px",
-                      aspectRatio: "1/1",
-                    }}
-                  >
-                    <GitHubIcon
-                      sx={{
-                        color: "#000000",
-                        height: isSmall ? "8px" : "32px",
-                        aspectRatio: "1/1",
-                      }}
-                    />
-                  </IconButton>
-                </Grid>
-              </div>
-            </Grid>
-
-            {/* //Location */}
-            <Grid container direction={"column"} xs alignContent={"end"}>
-              <Grid container direction={"row"} xs={12}>
-                <Grid xs={1}>
-                  {/* //Conditionally render LocationOnIcon  size based  off of isSmall  const */}
-                  <LocationOnIcon
-                    sx={{
-                      color: "#000000",
-                      mt: "5px",
-                      height: isSmall ? "8px" : "16px",
-                      width: isSmall ? "8px" : "16px",
-                    }}
-                  />
-                </Grid>
-                <Grid xs>
-                  <Typography
-                    textAlign={"start"}
-                    color="#C9FFC2"
-                    sx={{ mt: "5px" }}
-                    variant="body2">
-                    Smyrna, DE
-                  </Typography>
-                </Grid>
               </Grid>
-              <Grid item xs>
-                <Button
-                  variant="contained"
-                  href={Pdf}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  sx={{
-                    borderRadius: 5, // Adjust the radius as needed
-                    backgroundColor: "#D9D9D9",
-                    "&:hover": {
-                      backgroundColor: "#D9D9D9", 
-                    },
-                    width: isSmall ? "80%" : "100%",
-                  }}
-                  startIcon={
-                    <FileDownloadIcon
-                      sx={{
-                        color: "#000000",
-                        height: isSmall ? "8px" : "32px",
-                        aspectRatio: "1/1",
-                      }}
-                    />
-                  }
-                >
-                  <Typography
-                    variant="body2"
-                    color="#000000"
-                    sx={{ mr: "10px" }}
-                  >
-                    Resume
-                  </Typography>
-                </Button>
-              </Grid>
-            </Grid>
+              
           </Grid>
         </CardContent>
       </Card>
